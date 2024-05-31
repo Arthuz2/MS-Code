83e9b86481a495a7187ed596be2009afeb706580
Posino <125914429+Arthuz2@users.noreply.github.com>
2024-05-31 18:10:50 -0300

    Calculadora

---
+++ JavaScript/Calculadora/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    
+    $("#soma").click(function(){
+        var n1 = document.getElementById('num1').value;
+        var n2 = document.getElementById('num2').value;
+
+        n1 = parseInt(n1);
+        n2 = parseInt(n2);
+
+        var resultado = n1 + n2;
+        document.getElementById("text").innerHTML = resultado;
+    });
+
+    $("#subt").click(function(){
+        var n1 = document.getElementById('num1').value;
+        var n2 = document.getElementById('num2').value;
+
+        n1 = parseInt(n1);
+        n2 = parseInt(n2);
+
+        var resultado = n1 - n2;
+        document.getElementById("text").innerHTML = resultado;
+    })
+});
