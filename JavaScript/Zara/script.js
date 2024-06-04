092fb97d8834d63d4a1f5a336296e25fe1382ff9
unknown <arthur.porcino43@gmail.com>
2024-06-04 16:33:10 -0300

    CarrinhoZara

--- JavaScript/Zara/script.js
+++ JavaScript/Zara/script.js
@@ -1,3 +1,17 @@
+var total = 0;
+
+function adicionarAoCarrinho(item, preco) {
+    total += preco;
+    total = parseFloat(total.toFixed(2));
+    document.getElementById('totalPrice').innerHTML = total;
+    
+    const li = $("<li></li>");
+    const ctd = `<span class="titleItems">${item} (${numVezes[0]}x)</span><br>&emsp;<span class="priceItems">R$${preco}</span><br>`;
+    li.html(ctd);
+    $('#cartItems').append(ctd);
+    $('.titleItems').css({"font-size": "20px", "font-weigth": "300"}); $('.priceItems').css({"font-size": "15px"});
+}
+
 $(document).ready(function(){
 
     $('#saveButton').click(function(event){
@@ -8,12 +22,13 @@ $(document).ready(function(){
 
         if (nome != "" && email != "") {
             $('.add-to-cart').attr('disabled', false);   
-            $('#userInfo').show(function(){
-                $(this).append = `${nome} <br> ${email}`;
-            }); 
-
+            $('#userInfo').show(function(){;
+                const p = $('<p></p>');
+                const dados = `Nome : ${nome} <br> Email: ${email}`;
+                p.html(dados);
+                $('#userInfo').css({"display": "flex", "justify-content": "start", "font-weight": "500", "font-size": "20px"})
+                $('#userInfo').append(p);
+            });
         }
-        
-        
     });
 });
